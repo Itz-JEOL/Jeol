8711d5afbc7074f10d9208f0eecb654c1ab0a1ef
Jeol Paul <123471314+Itz-JEOL@users.noreply.github.com>
2023-03-03 11:56:29 +0530

    Update __init__.py

--- pyjeol/__init__.py
+++ pyjeol/__init__.py
@@ -4,3 +4,10 @@ def add(a,b):
     return a+b
 
 CMD_PREFIXES = ["/", "."]
+
+
+__version__ = "0.1"
+__description__ = "An add-on extension to Pyrogram for absolute beginners"
+__copyright__ = "Copyright (C) 2023 Jeol Paul"
+__license__ = "MIT License"
+__terms__ = f"Pyjeol is free software and comes with ABSOLUTELY NO WARRANTY. \nLicensed under the terms of {__license__}."
